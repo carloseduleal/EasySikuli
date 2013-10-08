e5036dd023ae6a0c299841b120db6fa67f35f514
unknown <cels@EMBARE.cesar.intranet>
2013-10-08 17:57:46 -0300

    melhorias, varias melhorias

--- src/IOactions/ScreenDriver.java
+++ src/IOactions/ScreenDriver.java
@@ -2,10 +2,13 @@ package IOactions;
 
 import java.io.File;
 
+import junit.framework.Assert;
+
 import org.sikuli.api.DesktopScreenRegion;
 import org.sikuli.api.ImageTarget;
 import org.sikuli.api.ScreenRegion;
 import org.sikuli.api.Target;
+import org.sikuli.api.robot.Key;
 import org.sikuli.api.robot.Keyboard;
 import org.sikuli.api.robot.Mouse;
 import org.sikuli.api.robot.desktop.DesktopKeyboard;
@@ -24,7 +27,8 @@ public class ScreenDriver {
 	PostFind find;
 	PostWait wait;
 
-	public PostWait waitFor(String imagePath, int timeInSeconds, double similarity) {
+	public PostWait waitFor(String imagePath, int timeInSeconds,
+			double similarity) {
 		int timeResult = 0;
 		ScreenRegion myDesktop = new DesktopScreenRegion();
 		File image = new File(imagePath);
@@ -35,8 +39,8 @@ public class ScreenDriver {
 		timeResult = timeInSeconds * 1000;
 		aux.wait(imageTarget, timeResult);
 		aux = myDesktop.find(imageTarget);
-		
-		//PosWaitAction
+
+		// PosWaitAction
 		wait = new PostWait();
 		wait.setImagePath(imagePath);
 		wait.setTimeInSeconds(timeInSeconds);
@@ -46,7 +50,7 @@ public class ScreenDriver {
 
 	public PostWait waitFor(String imagePath, int timeInSeconds) {
 		waitFor(imagePath, timeInSeconds, defaultSimilarity);
-		
+
 		wait = new PostWait();
 		wait.setImagePath(imagePath);
 		wait.setTimeInSeconds(timeInSeconds);
@@ -62,12 +66,9 @@ public class ScreenDriver {
 
 		ScreenRegion myScreen = myDesktop;
 		myScreen = myDesktop.find(imageTarget);
-	
-		Canvas canvas = new DesktopCanvas();
-		canvas.addBox(myScreen).withLineWidth(3);
-		canvas.display(2);
-		
-		//PosFind
+		myScreen.getCenter();
+
+		// PosFind
 		find = new PostFind();
 		find.setImagePath(imagePath);
 		find.setSimilarity(similarity);
@@ -76,11 +77,54 @@ public class ScreenDriver {
 
 	public PostFind findImage(String imagePath) {
 		findImage(imagePath, defaultSimilarity);
-		
+
 		find = new PostFind();
 		find.setImagePath(imagePath);
 		find.setSimilarity(defaultSimilarity);
 		return find;
 	}
 
+	public void findHightLight(String imagePath) {
+		ScreenRegion myDesktop = new DesktopScreenRegion();
+		File image = new File(imagePath);
+		Target imageTarget = new ImageTarget(image);
+		imageTarget.setMinScore(defaultSimilarity);
+
+		ScreenRegion myScreen = myDesktop;
+		myScreen = myDesktop.find(imageTarget);
+
+		Canvas canvas = new DesktopCanvas();
+		canvas.addBox(myScreen).withLineWidth(4);
+		canvas.display(5);
+	}
+
+	public void assertImageExists(String expectedImagePath, double similarity, int timeInSeconds) {
+		int timeResult;
+		ScreenRegion myDesktop = new DesktopScreenRegion();
+		File image = new File(expectedImagePath);
+		Target imageTarget = new ImageTarget(image);
+		imageTarget.setMinScore(similarity);
+
+		ScreenRegion myScreen = myDesktop;
+		timeResult = timeInSeconds *  1000;
+		myScreen.wait(imageTarget, timeResult);
+		myScreen = myDesktop.find(imageTarget);
+		if (myScreen == null) {
+			screenShot();
+		}
+		Assert.assertNotNull(myScreen);
+	}
+	
+	public void assertImageExists(String expectedImagePath, double similarity) {
+		assertImageExists(expectedImagePath, similarity, 20);
+	}
+	
+	public void assertImageExists(String expectedImagePath) {
+		assertImageExists(expectedImagePath, defaultSimilarity, 20);
+	}
+
+	public void screenShot() {
+		keyboard = new DesktopKeyboard();
+		keyboard.type(Key.PAUSE);
+	}	
 }
